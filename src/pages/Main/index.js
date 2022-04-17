69ed1715b50e0cf10a10bb59e65d96f95e5b12ee
Giovanny L. Silva <didiosilva395@gmail.com>
2022-04-17 12:28:20 -0400

    utilizando localStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,107 @@
-import React from "react";
+import React, { Component } from "react";
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-function Main(){
-  return <h1> Main </h1>
+import api from '../../services/api';
+
+import { Container, Form, SubmitButton, List } from './styles';
+
+export class Main extends Component{
+  state ={
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  componentDidMount(){
+    const repositories = localStorage.getItem('repositories');
+    if (repositories){
+      this.setState({ repositories: JSON.parse(repositories)});
+    }
+  }
+  componentDidUpdate(_, prevState){
+    const { repositories } = this.state;
+
+    if(prevState.repositories !== repositories){
+      localStorage.setItem('repositories',JSON.stringify(repositories));
+    }
+
+  }
+  
+
+  handleInputChange = e =>{
+    this.setState({ newRepo: e.target.value });
+  }
+
+  handleSubmit = async e =>{ 
+    e.preventDefault();
+    this.setState({loading: true});
+
+    const { newRepo } = this.state;
+    const response = await api.get(`/repos/${newRepo}`);
+    const data = {
+      name: response.data.full_name,
+    };
+    this.setState({
+      //repositories: [...repositories, data],
+      repositories: [...this.state.repositories,data],
+      newRepo: '',
+      loading:false,
+    });
+
+    console.log(newRepo);
+
+  }
+
+  render(){
+    //const { newRepo,loading } = this.setState;
+    /*console.log('T1');
+    console.log(newRepo);
+    console.log('T2');
+    console.log(loading);*/
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt/>
+          Repositórios
+        </h1>
+  
+        <Form onSubmit={this.handleSubmit}>
+          <input 
+            type="text" 
+            placeholder="Adicionar repositório"
+            //value={ newRepo }
+            value={this.state.newRepo}
+            onChange={this.handleInputChange}
+          />
+  
+          <SubmitButton loading={this.state.loading}>
+            {
+              this.state.loading ? (
+                <FaSpinner color="#fff" size={14}/>
+              ):(
+                <FaPlus color="#fff" size={14}/>)
+            }
+
+            
+          </SubmitButton>
+        </Form>
+
+        <List>
+            {
+              this.state.repositories.map(repository =>(
+                  <li key={repository.name}>
+                    <span>{repository.name} </span>
+                    <a href="#">Detalhes</a>
+                  </li>
+                )
+              )
+            }
+        </List>
+      </Container>
+  
+    );
+  }
 }
 
 export default Main;
