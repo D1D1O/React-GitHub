00710f9bb37da50aa34833c65066a7483812f05c
Giovanny L. Silva <didiosilva395@gmail.com>
2022-04-17 22:26:51 -0400

    Carregando dados da API

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import { useParams } from "react-router-dom";
-//import { useParams } from "react-router-dom";
+import api from "../../services/api";
+
+class Repository extends Component{
+
+  state = {
+    repository:{},
+    issues:[],
+    loading:true,
+  }
 
 
-export default function Repository({match}){
-  let { repository } = useParams(); 
-  //console.log(repository); 
-  return <h1> Repository: {repository} </h1>
+  async componentDidMount(){
+    let { repository } = this.props.params; 
+    const [repo, issues] = await Promise.all([
+      api.get(`/repos/${repository}`),
+      api.get(`/repos/${repository}/issues`,{
+        params:{
+          state: 'open',
+          per_page: 5
+        }
+      }),
+    ]);
+
+    this.setState({
+      repository: repo.data,
+      issues: issues.data,
+      loading: false,
+    });
+
+  }
+
+  render(){
+    
+    return <h1> Repository: </h1>
+  }
 }
+
+export default (props) => (
+  <Repository
+      {...props}
+      params={useParams()}
+  />
+);
+
+
+
