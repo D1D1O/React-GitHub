371d37fffb15dbb07b58fc54aeb8af334ed85b35
Giovanny L. Silva <didiosilva395@gmail.com>
2022-04-18 22:13:46 -0400

    repository

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import api from "../../services/api";
+//import PropTypes from 'prop-types';
+import { Container } from '../../components/Container';
+
+import { Loading, Owner , IssueList} from './styles';
 
 class Repository extends Component{
 
@@ -32,8 +37,45 @@ class Repository extends Component{
   }
 
   render(){
+
+    if(this.state.loading){
+      return (
+        <>
+          <Link to={"/"}>Voltar</Link>
+          <Loading> Carregando </Loading>
+          </>
+        )
+    }
     
-    return <h1> Repository: </h1>
+    return (
+      <Container>
+        <Owner>
+          <Link to={"/"}>Voltar</Link>
+          <img src={this.state.repository.owner.avatar_url} alt={this.state.repository.owner.login} ></img>
+          <h1> {this.state.repository.name} </h1>
+          <p> {this.state.repository.description} </p>
+        </Owner>
+
+        <IssueList>
+          { this.state.issues.map(issue => (
+            <li key={String(issue.id)}>
+             <img src={issue.user.avatar_url} alt={issue.user.login}></img>
+             <div>
+               <strong>
+                 <a href={issue.html_url} >{issue.title}</a>
+                 { issue.labels.map(label =>(
+                   <span key={String(label.id)} >{label.name} </span>
+                 )) }
+               </strong>
+               <p>{issue.user.login} </p>
+             </div>
+            </li>
+          ))
+          }
+        </IssueList>
+      </Container>
+
+    )
   }
 }
 
